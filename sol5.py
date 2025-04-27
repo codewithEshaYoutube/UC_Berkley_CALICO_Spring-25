4def412e7419507164ebfd1746c1d0a84f916d8b
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-04-27 07:05:42 +0500

    Update sol5.py

--- sol5.py
+++ sol5.py
@@ -1,22 +1,25 @@
-def count_total_uwu_subsequences(given_input_string):
+def count_uwu_subsequences_in_string(input_string):
 
-    list_of_u_letter_positions = []  # This list will store the positions (indexes) of all 'u' letters in the string
+    positions_of_u_characters = []  # This list will store the positions (indexes) of all 'u' characters in the string
     # Find and store positions of all 'u' characters
-    for current_character_index in range(len(given_input_string)):
-        if given_input_string[current_character_index] == 'u':
-            list_of_u_letter_positions.append(current_character_index)
-    total_uwu_subsequences_found = 0  # This will keep track of the number of "uwu" subsequences found
+    for current_char_index in range(len(input_string)):
+        if input_string[current_char_index] == 'u':
+            positions_of_u_characters.append(current_char_index)
+    
+    total_uwu_count = 0  # This will keep track of the number of "uwu" subsequences found
+    
     # Check all possible pairs of 'u' characters
-    for first_u_letter_index in range(len(list_of_u_letter_positions)):
-        for second_u_letter_index in range(first_u_letter_index + 1, len(list_of_u_letter_positions)):
+    for first_u_index in range(len(positions_of_u_characters)):
+        for second_u_index in range(first_u_index + 1, len(positions_of_u_characters)):
             # Now we check if there is any 'w' character between these two 'u' characters
-            for character_between_us_index in range(list_of_u_letter_positions[first_u_letter_index] + 1, list_of_u_letter_positions[second_u_letter_index]):
-                if given_input_string[character_between_us_index] == 'w':
-                    total_uwu_subsequences_found += 1  # If a 'w' is found, this forms a "uwu" subsequence
+            for index_between_us in range(positions_of_u_characters[first_u_index] + 1, positions_of_u_characters[second_u_index]):
+                if input_string[index_between_us] == 'w':
+                    total_uwu_count += 1  # If a 'w' is found, this forms a "uwu" subsequence
                     break  # No need to search further between this pair once we find one 'w'
-    return total_uwu_subsequences_found
+    
+    return total_uwu_count
 
-list_of_input_strings = ["uwwu", "uyuwuuxuwu", "uuwuu", "wwuuouw"]  # List of strings to test
-for current_test_string in list_of_input_strings:
-    uwu_subsequence_count = count_total_uwu_subsequences(current_test_string)
-    print(f'The number of "uwu" subsequences in "{current_test_string}" is: {uwu_subsequence_count}')
+input_test_strings = ["uwwu", "uyuwuuxuwu", "uuwuu", "wwuuouw"]  # List of strings to test
+for current_test_string in input_test_strings:
+    uwu_count_result = count_uwu_subsequences_in_string(current_test_string)
+    print(f'The number of "uwu" subsequences in "{current_test_string}" is: {uwu_count_result}')
